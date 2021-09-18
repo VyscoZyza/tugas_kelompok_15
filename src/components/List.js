8c443d5fd977362ba8eb8163e6a880a163ac0fda
Alvin Zulham <50919691+VyscoZyza@users.noreply.github.com>
2021-09-18 07:41:28 +0700

    final version

--- src/components/List.js
+++ src/components/List.js
@@ -1,50 +1,155 @@
-import React, { Component } from "react";
-import { Col, ListGroup } from "react-bootstrap";
-import axios from "axios";
+import React, { useState } from "react";
+import { Badge, Card, Col, ListGroup, Row } from "react-bootstrap";
+import { numberWithCommas } from "../utils/utils";
+import Keranjang from "./Keranjang";
+import TotalBayar from "./TotalBayar";
 import { API_URL } from "../utils/constants";
+import axios from "axios";
+import swal from "sweetalert";
 
-export default class List extends Component {
-  constructor(props) {
-    super(props);
+export default function Hasil(props) {
+  const [showModal, setShowModal] = useState(false);
+  const [keranjangDetail, setKeranjangDetail] = useState(false);
+  const [jumlah, setJumlah] = useState(0);
+  const [keterangan, setKeterangan] = useState("");
+  const [totalHarga, setTotalHarga] = useState(0);
 
-    this.state = {
-      categories: [],
+  const handleShow = (menuKeranjang) => {
+    setShowModal(true);
+    setKeranjangDetail(menuKeranjang);
+    setJumlah(menuKeranjang.jumlah);
+    setKeterangan(menuKeranjang.keterangan);
+    setTotalHarga(menuKeranjang.total_harga);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const tambah = () => {
+    setJumlah(jumlah + 1);
+    setTotalHarga(keranjangDetail.product.harga * (jumlah + 1));
+  };
+
+  const kurang = () => {
+    if (jumlah !== 1) {
+      setJumlah(jumlah - 1); // why coma
+      setTotalHarga(keranjangDetail.product.harga * (jumlah - 1));
+    }
+  };
+
+  const changeHandler = (event) => {
+    setKeterangan(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    handleClose();
+
+    const data = {
+      jumlah: jumlah,
+      total_harga: totalHarga,
+      product: keranjangDetail.product,
+      keterangan: keterangan,
     };
-  }
 
-  componentDidMount() {
     axios
-      .get(API_URL + "categories")
+      .put(API_URL + "keranjangs/" + keranjangDetail.id, data)
       .then((res) => {
-        const categories = res.data;
-        this.setState({ categories });
+        swal({
+          title: "Update Pesanan!",
+          text: "Sukses Update Pesanan " + data.product.nama,
+          icon: "success",
+          button: false,
+          timer: 1500,
+        });
       })
       .catch((error) => {
         console.log("Error yaa ", error);
       });
-  }
+  };
 
-  render() {
-    const { categories } = this.state;
-    const { changeCategory, categoriYangDipilih } = this.props;
-    return (
-      <Col md={2} className="mt-3">
-        <ListGroup horizontal>
-          {categories &&
-            categories.map((category) => (
+  const hapusPesanan = (id) => {
+    handleClose();
+
+    axios
+      .delete(API_URL + "keranjangs/" + id)
+      .then((res) => {
+        swal({
+          title: "Hapus Pesanan!",
+          text: "Sukses Hapus Pesanan " + keranjangDetail.product.nama,
+          icon: "error",
+          button: false,
+          timer: 1500,
+        });
+      })
+      .catch((error) => {
+        console.log("Error yaa ", error);
+      });
+  };
+  console.log(keranjangs);
+  const { keranjangs } = useState(props);
+  return (
+    <Col md={3} className="mt-3">
+      <h4>
+        <strong>Keranjang</strong>
+      </h4>
+
+      {keranjangs.length !== 0 && (
+        <Card className="overflow-auto hasil ">
+          <ListGroup variant="flush">
+            <Row className="category-aktif">
+              <Col xs={2} className="ml-3 mt-2">
+                <h5 className="ml-2">Jml</h5>
+              </Col>
+              <Col>
+                <h5 className="mt-2">Produk</h5>
+              </Col>
+              <Col>
+                <strong className="float-right mr-3 mt-2">Total Harga</strong>
+              </Col>
+            </Row>
+            {keranjangs.map((menuKeranjang) => (
               <ListGroup.Item
-                key={category.id}
-                onClick={() => changeCategory(category.nama)}
-                className={
-                  categoriYangDipilih === category.nama && "category-aktif"
-                }
-                style={{ cursor: "pointer" }}
+                key={menuKeranjang.id}
+                onClick={() => handleShow(menuKeranjang)}
               >
-                <h5>{category.nama}</h5>
+                <Row>
+                  <Col xs={2}>
+                    <h4>
+                      <Badge pill variant="primary">
+                        {menuKeranjang.jumlah}
+                      </Badge>
+                    </h4>
+                  </Col>
+                  <Col>
+                    <h5>{menuKeranjang.product.nama}</h5>
+                    <p>Rp. {numberWithCommas(menuKeranjang.product.harga)}</p>
+                  </Col>
+                  <Col>
+                    <strong className="float-right">
+                      Rp. {numberWithCommas(menuKeranjang.total_harga)}
+                    </strong>
+                  </Col>
+                </Row>
               </ListGroup.Item>
             ))}
-        </ListGroup>
-      </Col>
-    );
-  }
+
+            <Keranjang
+              handleClose={handleClose}
+              {...props}
+              tambah={tambah}
+              kurang={kurang}
+              changeHandler={changeHandler}
+              handleSubmit={handleSubmit}
+              hapusPesanan={hapusPesanan}
+            />
+          </ListGroup>
+        </Card>
+      )}
+
+      <TotalBayar keranjangs={keranjangs} {...this.props} />
+    </Col>
+  );
 }
